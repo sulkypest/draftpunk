7bbff771787f1a893b7ac0d2538149f3b6da5a5f
sulkypest <146014342+sulkypest@users.noreply.github.com>
2026-03-30 09:16:59 +0100

    Update sentry.js

--- sentry.js
+++ sentry.js
@@ -2,9 +2,9 @@
 // 1. Sign up at https://sentry.io (free tier)
 // 2. Create a new project → Browser JavaScript
 // 3. Copy your DSN and paste it below
-const SENTRY_DSN = 'YOUR_SENTRY_DSN_HERE';
+const SENTRY_DSN = 'https://js-de.sentry-cdn.com/f663d78c5f2fba08ba41cc3ccf663cbc.min.js';
 
-if (SENTRY_DSN !== 'YOUR_SENTRY_DSN_HERE') {
+if (SENTRY_DSN !== 'https://js-de.sentry-cdn.com/f663d78c5f2fba08ba41cc3ccf663cbc.min.js') {
     const script    = document.createElement('script');
     script.src      = 'https://browser.sentry-cdn.com/8.28.0/bundle.min.js';
     script.crossOrigin = 'anonymous';
